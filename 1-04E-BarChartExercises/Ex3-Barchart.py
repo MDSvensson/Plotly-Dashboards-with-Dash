276cdb47a574b33a2d01411fcc9e97536758839f
Marc Svensson <m82svensson@hotmail.com>
2021-04-06 12:08:38 +0100

    barchart exercise

--- 1-04E-BarChartExercises/Ex3-Barchart.py
+++ 1-04E-BarChartExercises/Ex3-Barchart.py
@@ -6,24 +6,26 @@
 ######
 
 # Perform imports here:
-
-
+from pandas.core.indexes.base import Index
+import plotly.offline as pyo
+import plotly.graph_objs as go
+import pandas as pd
 
 
 # create a DataFrame from the .csv file:
-
+df = pd.read_csv('../Data/mocksurvey.csv',
+                index_col=0)
 
 # create traces using a list comprehension:
-
-
-
-
-
+traces = [go.Bar(
+    x=df.index,
+    y=df[name],
+    name=name) for name in df.columns] # set the marker color to bronze
 
 # create a layout, remember to set the barmode here
-
-
-
-
+layout = go.Layout(title={'text':'Mock Survey Results', 'y':0.9, 'x':0.5,},
+                            barmode='stack')
 
 # create a fig from data & layout, and plot the fig.
+fig = go.Figure(data=traces,layout=layout)
+pyo.plot(fig, filename='barchart_exercise.html')

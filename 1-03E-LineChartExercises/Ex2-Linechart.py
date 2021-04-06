0bfcc8018a84ee5df2dd3f66bdad079dcef94e4c
Marc Svensson <m82svensson@hotmail.com>
2021-04-06 09:32:44 +0100

    linechart exercise

--- 1-03E-LineChartExercises/Ex2-Linechart.py
+++ 1-03E-LineChartExercises/Ex2-Linechart.py
@@ -5,22 +5,35 @@
 ######
 
 # Perform imports here:
-
-
+import plotly.offline as pyo
+import plotly.graph_objs as go
+import pandas as pd
 
 
 # Create a pandas DataFrame from 2010YumaAZ.csv
 df = pd.read_csv('../data/2010YumaAZ.csv')
+df.set_index('DAY', inplace=True)
+
 days = ['TUESDAY','WEDNESDAY','THURSDAY','FRIDAY','SATURDAY','SUNDAY','MONDAY']
 
-
 # Use a for loop (or list comprehension to create traces for the data list)
-data = []
+data = [go.Scatter(
+    x=df['LST_TIME'].loc[day],
+    y=df['T_HR_AVG'].loc[day],
+    mode='lines',
+    name=day
+)
+for day in days]
 
-for day in days:
-    # What should go inside this Scatter call?
-    trace = go.Scatter()
-    data.append(trace)
+layout = go.Layout(title='Temperature variation per week day')
+
+fig = go.Figure(data=data,layout=layout)
+pyo.plot(fig, filename='line_exercise.html')
+
+# for day in days:
+#     # What should go inside this Scatter call?
+#     trace = go.Scatter(x=, y=, mode=, name=)
+#     data.append(trace)
 
 # Define the layout
 

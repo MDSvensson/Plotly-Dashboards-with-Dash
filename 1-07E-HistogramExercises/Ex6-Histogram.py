c3b4946f4a0f8fc387034f506b813cb89f86ace5
Marc Svensson <m82svensson@hotmail.com>
2021-04-07 12:19:10 +0100

    histogram exercise

--- 1-07E-HistogramExercises/Ex6-Histogram.py
+++ 1-07E-HistogramExercises/Ex6-Histogram.py
@@ -5,22 +5,25 @@
 ######
 
 # Perform imports here:
-
-
+import plotly.offline as pyo
+import plotly.graph_objs as go
+import pandas as pd
 
 
 # create a DataFrame from the .csv file:
-
-
+df = pd.read_csv('../data/abalone.csv')
+print(df)
 # create a data variable:
-
-
-
-
+data = [go.Histogram(
+    x=df['length'],
+    xbins=dict(start=0,end=1,size=0.02),
+)]
 
 # add a layout
-
-
-
+layout = go.Layout(
+    title=dict(text="Abalone length frequency",x=0.5)
+)
 
 # create a fig from data & layout, and plot the fig
+fig = go.Figure(data=data, layout=layout)
+pyo.plot(fig, filename='histogram_exercise.html')

487b05237b72f1a20663466de78d5fdf114802b0
Marc Svensson <m82svensson@hotmail.com>
2021-04-08 16:44:03 +0100

    simple dashboard exercise

--- 2-03-SimpleDashboardExercise/Ex1-SimpleDashboard.py
+++ 2-03-SimpleDashboardExercise/Ex1-SimpleDashboard.py
@@ -8,20 +8,51 @@
 ######
 
 # Perform imports here:
-
-
-
-
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
+import numpy as np
 
 
 # Launch the application:
-
+app = dash.Dash()
 
 # Create a DataFrame from the .csv file:
-
+df = pd.read_csv('../Data/OldFaithful.csv')
+print(df)
 
 # Create a Dash layout that contains a Graph component:
+app.layout = html.Div([
+    dcc.Graph(
+        id='scatter',
+        figure={
+            'data': [
+                go.Scatter(
+                    x = df['X'],
+                    y = df['Y'],
+                    mode = 'markers',
+                    marker = {
+                        'size': 12,
+                        'color': 'rgb(51,204,153)',
+                        'symbol': 'circle',
+                        'line': {'width': 2}
+                        }
+                )
+            ],
+            'layout': go.Layout(
+                title = 'Old Faithful Eruption Interval v Duration',
+                xaxis = {'title': 'Some random x-values'},
+                yaxis = {'title': 'Some random y-values'},
+                hovermode='closest'
+            )
+        }
+    )
+])
 
+if __name__ == '__main__':
+    app.run_server()
 
 
 
